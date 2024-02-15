8f869f534fa6c82302ac928711921baf7415e4a8
henrytheweeb <henrique.netslayer@gmail.com>
2024-02-15 19:19:34 +0000

    conversion script finished csv data generated for Dark Photon and Dark Higgs and converted successfuly

--- scripts/csv_to_sndsw.py
+++ scripts/csv_to_sndsw.py
@@ -1,15 +1,25 @@
 import pandas as pd
 import numpy as np
 import ROOT
+import sys
 
 # Read the CSV file into a pandas DataFrame
-df = pd.read_csv('input.csv')
+
+#argv[0]:script name
+#argv[1]:csvfile
+#argv[2]:outputfile
+
+if len(sys.argv)!=3:raise Exception("Wrong number of arguments: needs 2, given {}".format(len(sys.argv)-1))
+if not sys.argv[1].endswith(".csv"):raise Exception("argument 1 must end in .csv")
+if not sys.argv[2].endswith(".root"):raise Exception("argument 2 must end in .root")
+    
+df = pd.read_csv(sys.argv[1])
 
 # Create a ROOT file to store the TTree
-output_file = ROOT.TFile('output.root', 'RECREATE')
+output_file = ROOT.TFile(sys.argv[2], 'RECREATE')
 
 # Create a TTree
-tree = ROOT.TTree('tree', 'Data')
+tree = ROOT.TTree('gst', 'gst')
 
 # Create branches in the TTree for each variable
 Ev = np.zeros(1, dtype=float)
@@ -32,7 +42,7 @@ pxf = np.zeros(2, dtype=float)
 pyf = np.zeros(2, dtype=float)
 pzf = np.zeros(2, dtype=float)
 nf = np.zeros(1, dtype=int)
-pdgf = np.zeros(2, dtype=int)
+pdgf = np.zeros(2, dtype=np.int32)
 
 tree.Branch('Ev', Ev, 'Ev/D')
 tree.Branch('pxv', pxv, 'pxv/D')
@@ -57,35 +67,41 @@ tree.Branch('nf', nf, 'nf/I')
 tree.Branch('pdgf', pdgf, 'pdgf[2]/I')
 
 # Fill the TTree with the data from the DataFrame
-for index, row in df.iterrows():
-    Ev[0] = row['Ev']
-    pxv[0] = row['pxv']
-    pyv[0] = row['pyv']
-    pzv[0] = row['pzv']
-    neu[0] = row['neu']
-    cc[0] = row['cc']
-    nuel[0] = row['nuel']
-    vtxx[0] = row['vtxx']
-    vtxy[0] = row['vtxy']
-    vtxz[0] = row['vtxz']
-    vtxt[0] = row['vtxt']
-    El[0] = row['El']
-    pxl[0] = row['pxl']
-    pyl[0] = row['pyl']
-    pzl[0] = row['pzl']
-    Ef[0] = row['Ef'][0]
-    Ef[1] = row['Ef'][1]
-    pxf[0] = row['pxf'][0]
-    pxf[1] = row['pxf'][1]
-    pyf[0] = row['pyf'][0]
-    pyf[1] = row['pyf'][1]
-    pzf[0] = row['pzf'][0]
-    pzf[1] = row['pzf'][1]
-    nf[0] = row['nf']
-    pdgf[0] = row['pdgf'][0]
-    pdgf[1] = row['pdgf'][1]
-    tree.Fill()
-
+for index, row in df[df['particle_type']==32].iterrows():
+     #print("index {}\n row {} \n".format(index,row))
+    #print(df.iloc[index+1])
+    rowmu=df.iloc[index+1]
+    rowmu2=df.iloc[index+2]
+    if rowmu['particle_type']==-rowmu2['particle_type']:
+        Ev[0] = np.sqrt(row['px']**2+row['py']**2+row['pz']**2+row['m']**2)
+        pxv[0] = row['px']
+        pyv[0] = row['py']
+        pzv[0] = row['pz']
+        neu[0] = row['particle_type']
+        cc[0] = False
+        nuel[0] = False
+        vtxx[0] = row['vx']/1000
+        vtxy[0] = row['vy']/1000
+        vtxz[0] = row['vz']/1000-10
+        vtxt[0] = row['vt']/1000-10
+        El[0] = np.sqrt(row['px']**2+row['py']**2+row['pz']**2+row['m']**2)
+        pxl[0] = row['px']
+        pyl[0] = row['py']
+        pzl[0] = row['pz']
+        Ef[0] = np.sqrt(rowmu['px']**2+rowmu['py']**2+rowmu['pz']**2+rowmu['m']**2)
+        Ef[1] = np.sqrt(rowmu2['px']**2+rowmu2['py']**2+rowmu2['pz']**2+rowmu2['m']**2)
+        pxf[0] = rowmu['px']
+        pxf[1] = rowmu2['px']
+        pyf[0] = rowmu['py']
+        pyf[1] = rowmu2['py']
+        pzf[0] = rowmu['pz']
+        pzf[1] = rowmu2['pz']
+        nf[0] = 2
+        pdgf[0]=rowmu['particle_type']
+        pdgf[1]=rowmu2['particle_type']
+        tree.Fill()
+        
+print("Tree fill concluded, saving to output file")  
 # Write the TTree to the output file
 output_file.Write()
 output_file.Close()
